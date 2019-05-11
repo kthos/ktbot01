c30a9d79754c4d942e260bb21a7b221b3ce45a07
kthos <klongthomhospital@gmail.com>
2019-05-11 18:32:48 +0700

    Line Ktbot01 App For test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const line = require('@line/bot-sdk');
 const express = require('express');
 const config = require('./config.json');
 
-// create LINE SDK client  nont
+// create LINE SDK client
 const client = new line.Client(config);
 
 const app = express();
